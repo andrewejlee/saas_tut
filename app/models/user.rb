9c4ff78c1675c020bafec177b1d1deac23ae7e1a
Andrew Lee <andrew.lee.ej.6@gmail.com>
2016-12-21 01:35:55 +0000

    Added profile and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   
   belongs_to :plan
+  has_one :profile
+
+  
   attr_accessor :stripe_card_token
   # If pro user passes validation (email, pw etc)
   # then call Stripe and tell Stripe to set up subscription
