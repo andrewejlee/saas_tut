9213e5a9ebe982fa07267dcabf9de5cd516b3366
Andrew Lee <andrew.lee.ej.6@gmail.com>
2017-01-10 22:09:22 +0000

    added helper for job title icon

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,13 @@
+module UsersHelper
+  def job_title_icon
+    if @user.profile.job_title == "Developer"
+      "<i class='fa fa-code-o'></i>".html_safe
+    elsif @user.profile.job_title == "Entrepreneur"
+      "<i class='fa fa-light-bulb-o'></i>".html_safe
+    elsif @user.profile.job_title == "Investor"
+      "<i class='fa fa-dollar'></i>".html_safe
+    elsif @user.profile.job_title == "Designer"
+      "<i class='fa fa-paint-brush'></i>".html_safe
+    end
+  end
+end
