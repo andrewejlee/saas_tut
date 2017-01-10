268e36c25195a95289d0fffdae1a5686203f9f56
Andrew Lee <andrew.lee.ej.6@gmail.com>
2017-01-10 23:23:06 +0000

    added profile update action

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def job_title_icon
     if @user.profile.job_title == "Developer"
-      "<i class='fa fa-code-o'></i>".html_safe
+      "<i class='fa fa-file-code-o'></i>".html_safe
     elsif @user.profile.job_title == "Entrepreneur"
-      "<i class='fa fa-light-bulb-o'></i>".html_safe
+      "<i class='fa fa-lightbulb-o'></i>".html_safe
     elsif @user.profile.job_title == "Investor"
       "<i class='fa fa-dollar'></i>".html_safe
     elsif @user.profile.job_title == "Designer"
